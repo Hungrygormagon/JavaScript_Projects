398502d2e3675023a2ba387884a52379fd17b682
Steven <stevenjcarney@gmail.com>
2020-05-03 12:37:16 -0700

    Intial Commit

---
+++ Basic Javascript Projects/JS/Basic_Javascript_2.js
@@ -0,0 +1,11 @@
+//function Brown_Fox () {
+ //   var fox= "The quick brown fox";
+ //   var result= "Goodbye";
+ //   document.getElementById("lazydog").innerHTML = result;
+//}
+
+function functionOne(){
+    var sentence = 'I am learning';
+    sentence += " a lot from this course!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
