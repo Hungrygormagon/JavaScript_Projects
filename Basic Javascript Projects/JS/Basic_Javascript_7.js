dad6fc90160e618eeb4348176ed722ee299ddcbb
Steven <stevenjcarney@gmail.com>
2020-05-05 10:13:37 -0700

    Additional Drill work

---
+++ Basic Javascript Projects/JS/Basic_Javascript_7.js
@@ -0,0 +1,39 @@
+function get_Date() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 ==Time >0){
+        Reply= "Good Morning!";
+    }
+    else if (Time >12 == Time < 18){
+        Reply="Good Afternoon";
+    }
+    else {
+        Reply="Good Evening.";
+    }
+        document.getElementById("Greeting").innerHTML=Reply;
+    
+};
+
+function vote_Function() {
+    var Age, Can_Vote;
+    Age=document.getElementById("Age").value;
+    if (Age>=18){
+        Can_Vote="You are mature enough to vote during this election.";
+    }
+    else if (Age<18) {
+        Can_Vote="You are too young to vote during this election.";
+    }
+    else {
+        Can_Vote="Please enter your age.";
+    }
+    document.getElementById("Vote").innerHTML = Can_Vote;
+};
+
+function add(){
+    var x=4;
+    document.getElementById("Add").innerHTML=x+4;
+};
+
+function Sub(){
+    console.log(x-906);
+}
